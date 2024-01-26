29d1affb3d32d6c8fe9b54de98a0459d8379271c
Yimkumer <yimkumerpongen@gmail.com>
2024-01-26 10:51:15 +0530

    vercel

--- server/db.js
+++ server/db.js
@@ -1,11 +1,16 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "root",
+//   host: "localhost",
+//   port: 5432,
+//   database: "mec"
+// });
+
+//VERCEL POSTGRESQL connection
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
-  port: 5432,
-  database: "mec"
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool;
